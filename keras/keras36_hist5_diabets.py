051da81a1296281ae98046a8dcf7abcd1c5c87db
HYUNMIN-HWANG <hwangkei0212@gmail.com>
2021-01-07 20:47:55 +0900

    21-01-07
    Former-commit-id: fd62f8f5cac5ca38a5189caa4da55633f798dc5c

--- keras/keras36_hist5_diabets.py
+++ keras/keras36_hist5_diabets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28a951e1c6a70b7ff278d3b1fc903495a7a397de5e4f95960325ac02c7b65ff7
-size 2312
+oid sha256:0b80c7b835d2f18fea1b397f0b9c8161db1f6173c08e84e227db7b5bfce8acc1
+size 2176
