e5f2062f1872684a70055670d5dab8ae2ef1569f
HYUNMIN-HWANG <70581043+HYUNMIN-HWANG@users.noreply.github.com>
2021-01-09 00:26:20 +0900

    21-01-09

--- keras/keras40_mnist3_dnn.py
+++ keras/keras40_mnist3_dnn.py
@@ -10,60 +10,57 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# print(x_train.shape, y_train.shape) # (60000, 28, 28)--> 흑백 1 생략 가능 (60000,) 
-# print(x_test.shape, y_test.shape)   # (10000, 28, 28)                     (10000,)  > 0 ~ 9 다중 분류
+# print(x_train.shape, y_train.shape) # (60000, 28, 28) (60000,) 
+# print(x_test.shape, y_test.shape)   # (10000, 28, 28) (10000,) 
 
 # print(x_train[0])   
 # print("y_train[0] : " , y_train[0])   # 5
 # print(x_train[0].shape)               # (28, 28)
 
-# plt.imshow(x_train[0], 'gray')  # 0 : black, ~255 : white (가로 세로 색깔)
-# plt.imshow(x_train[0]) # 색깔 지정 안해도 나오긴 함
-# plt.show()  
-
-# preprocessing
+# x > preprocessing
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2]).astype('float32')/255. 
 # 4차원 만들어준다. float타입으로 바꾸겠다. -> /255. -> 0 ~ 1 사이로 수렴됨
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1]*x_test.shape[2])/255. 
 
-print(x_train.shape)    # (60000, 784)
-print(x_test.shape)     # (10000, 784)
+# print(x_train.shape)    # (60000, 784)
+# print(x_test.shape)     # (10000, 784)
 
+# y > preprocessing
 # print(y_train)
 # print(y_test)
 # print(y_train.shape)    # (60000, )
 # print(y_test.shape)     # (10000, )
 
+from tensorflow.keras.utils import to_categorical
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+print(y_train.shape)    # (60000, 10)
+print(y_test.shape)     # (10000, 10)
 
 #2. Modling
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 
 model = Sequential()
-model.add(Conv2D(filters=64, kernel_size=(2,2), padding='same', strides=1, input_shape=(x_train.shape[1]*x_train.shape[2],)))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.1))
-model.add(Conv2D(filters=64, kernel_size=(4,4), padding='same', strides=1))
-model.add(MaxPooling2D(pool_size=3))
-model.add(Dropout(0.1))
-model.add(Flatten())
-
-model.add(Dense(16))
-model.add(Dense(10))
-
+model.add(Dense(196, input_shape=(x_train.shape[1],), activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(56))
+model.add(Dropout(0.2))
+model.add(Dense(56))
+model.add(Dense(28))
+model.add(Dense(10, activation='softmax'))
 model.summary()
 
-
 # Compile, Train
-# from tensorflow.keras.callbacks import EarlyStopping
-# es = EarlyStopping(monitor='acc', patience=5, mode='max')
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='acc', patience=5, mode='max')
 
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['acc'])
-# model.fit(x_train, y_train, epochs=10, batch_size=64, validation_split=0.2, callbacks=[es])
-model.fit(x_train, y_train, epochs=9, batch_size=64, validation_split=0.3)
+model.fit(x_train, y_train, epochs=100, batch_size=32, validation_split=0.2, callbacks=[es])
+# model.fit(x_train, y_train, epochs=9, batch_size=64, validation_split=0.3)
 
 # Evaluate, Predict
-loss, acc = model.evaluate(x_test, y_test, batch_size=64)
+loss, acc = model.evaluate(x_test, y_test, batch_size=32)
 print("loss : ",loss)
 print("acc : ",acc)
 
@@ -78,11 +75,19 @@ print(np.argmax(y_test[:10],axis=1))
 y_predict = model.predict(x_test[:10])
 print("y_pred[:10] :")  
 print(np.argmax(y_predict,axis=1))
- 
+
+# CNN
 # loss :  0.034563612192869186
 # acc :  0.9889000058174133
 # y_test[:10] :
 # [7 2 1 0 4 1 4 9 5 9]
 # y_pred[:10] :
 # [7 2 1 0 4 1 4 9 5 9]
-'''
+
+# DNN
+# loss :  0.11503560841083527
+# acc :  0.9812999963760376
+# y_test[:10] :
+# [7 2 1 0 4 1 4 9 5 9]
+# y_pred[:10] :
+# [7 2 1 0 4 1 4 9 6 9]
--- keras/keras40_mnist4_lstm.py
+++ keras/keras40_mnist4_lstm.py
@@ -3,3 +3,82 @@
 # LSTM 모델로 구성 input_shape=(28*14,2)
 # LSTM 모델로 구성 input_shape=(28*7,4)
 # LSTM 모델로 구성 input_shape=(7*7,16)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.datasets import mnist
+
+#1. DATA
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# print(x_train.shape)    # (60000, 28, 28)
+# print(x_test.shape)     # (10000, 28, 28)
+# print(y_train.shape)    # (60000,)
+# print(y_test.shape)     # (10000,)
+
+# print(x_train[0])
+# print(y_train[0])   #--> 5
+
+# x > preprocessing
+# print(np.min(x_train))  # --> 0
+# print(np.max(x_train))  # --> 255
+x_train = x_train.reshape(x_train.shape[0],28*14,2) / 255.  
+x_test = x_test.reshape(x_test.shape[0],28*14,2) / 255.    
+
+# print(x_train.shape)    # (60000, 784, 1)
+# print(x_test.shape)     # (10000, 784, 1)
+# print(np.min(x_train))  # --> 0.0
+# print(np.max(x_train))  # --> 1.0
+
+# y > preprocessing
+# print(y_train[:10])       # --> 0 부터 9까지 다중분류
+
+from tensorflow.keras.utils import to_categorical
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+# print(y_train[:10])
+# print(y_test[:10])
+# print(y_train.shape)    # (60000, 10)
+# print(y_test.shape)     # (10000, 10)
+
+#2. Modeling
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+
+model = Sequential()
+model.add(LSTM(5, input_shape=(28*14,2), activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(5))
+model.add(Dropout(0.1))
+model.add(Dense(5))
+model.add(Dropout(0.1))
+model.add(Dense(2))
+model.add(Dropout(0.1))
+model.add(Dense(10, activation='softmax'))
+
+# model.summary()
+
+#3. Compile, Train
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='acc', patience=8, mode='max')
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.fit(x_train, y_train, epochs=2, batch_size=32, validation_split=0.2, callbacks=[es])
+
+#4. Evaluate, Predict
+loss, acc = model.evaluate(x_test, y_test, batch_size=32)
+print("loss : ", loss)
+print("acc : ", acc)
+
+print("y_test : ")
+print(np.argmax(y_test[:10], axis=1))
+
+y_pred = model.predict(x_test[:10])
+print("y_predict : ")
+print(np.argmax(y_pred,axis=1))
+
+# LSTM
+# loss :  1.8663101196289062
+# acc :  0.27720001339912415
