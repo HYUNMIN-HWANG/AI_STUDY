9166cff8173d3a99da3d1d151a9b364ffb0b386c
HYUNMIN-HWANG <hwangkei0212@gmail.com>
2021-02-09 20:19:00 +0900

    21-02-09

--- keras2/keras67_2_male_female2.py
+++ keras2/keras67_2_male_female2.py
@@ -6,6 +6,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dropout, MaxPool2D, BatchNormalization, AveragePooling2D, Activation
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from keras.optimizers import Adam
 
 #1. DATA
 # npy load
@@ -14,20 +15,20 @@ x_valid = np.load('../data/image/gender/npy/keras67_valid_x.npy')
 y_train = np.load('../data/image/gender/npy/keras67_train_y.npy')
 y_valid = np.load('../data/image/gender/npy/keras67_valid_y.npy')
 
-x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, train_size=0.9, shuffle=True, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, train_size=0.95, shuffle=True, random_state=42)
 
-print(x_train.shape, x_valid.shape, x_test.shape)  # (1250, 56, 56, 3) (347, 56, 56, 3) (139, 56, 56, 3)
-print(y_train.shape, y_valid.shape, y_test.shape)  # (1250,) (347,) (139,)
+print(x_train.shape, x_valid.shape, x_test.shape)  # (1319, 56, 56, 3) (347, 56, 56, 3) (70, 56, 56, 3)
+print(y_train.shape, y_valid.shape, y_test.shape)  # (1319,) (347,) (70,)
 
 #2. Modeling
 model = Sequential()
-model.add(Conv2D(32, (2,2), padding='same', input_shape=(56, 56, 3)))
+model.add(Conv2D(128, (2,2), padding='same', input_shape=(56, 56, 3)))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
-model.add(Conv2D(32, (2,2), padding='same'))
+model.add(Conv2D(128, (2,2), padding='same'))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
-model.add(Conv2D(32, (2,2), padding='same'))
+model.add(Conv2D(128, (2,2), padding='same'))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
 model.add(AveragePooling2D(2,2))
@@ -44,21 +45,11 @@ model.add(Activation('relu'))
 model.add(BatchNormalization())
 model.add(AveragePooling2D(2,2))
 model.add(Dropout(0.2))
-
-model.add(Conv2D(128, (2,2), padding='same'))
-model.add(Activation('relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(128, (2,2), padding='same'))
-model.add(Activation('relu'))
-model.add(BatchNormalization())
-model.add(AveragePooling2D(2,2))
-model.add(Dropout(0.3))
 
 model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
-model.add(Dropout(0.3))
 model.add(Dense(32))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
@@ -66,11 +57,14 @@ model.add(Dense(1, activation='sigmoid'))
 
 #3. Compile, Train
 es = EarlyStopping(monitor='val_loss', patience=50, mode='min')
-lr = ReduceLROnPlateau(monitor='val_loss', factor=0.4, patience=20, mode='min')
+lr = ReduceLROnPlateau(monitor='val_loss', factor=0.4, patience=30, mode='min')
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.01), metrics=['acc'])
 model.fit(x_train, y_train, epochs=1000, batch_size=8, validation_data=(x_valid, y_valid), callbacks=[es, lr])
 
 loss, acc = model.evaluate(x_test, y_test, batch_size=8)
 print("loss : ", loss)
 print("acc : ", acc)
+
+# loss :  0.7593396902084351
+# acc :  0.5539568066596985
