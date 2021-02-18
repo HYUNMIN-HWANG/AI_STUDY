8039aa7421bb0203eef5e48ccf59085af9777d9e
HYUNMIN-HWANG <hwangkei0212@gmail.com>
2021-02-18 17:14:17 +0900

    21-02-18 import

--- python_import/p73_class.py
+++ python_import/p73_class.py
@@ -3,8 +3,8 @@
 class Person :
     def __init__(self, name, age, address) : 
         # __init__ : 클래스 초기화, 가장 먼저 실행된다.
-        # self : 반드시 명시되어야 함 , 클래스 자체, 자기 자신을 의미함, 따로 명시할 필요는 없음
-        # name, age, address : 입력 받는 값
+        # self : 반드시 명시되어야 함 , 클래스 자체, 자기 자신을 의미함 / 선언 할 때는 따로 명시할 필요는 없음
+        # name, age, address : 입력 받야야 하는 값
         self.name = name    # 클래스 내에서 사용 가능함
         self.age = age
         self.address = address
