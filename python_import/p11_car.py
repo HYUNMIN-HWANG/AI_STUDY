b92ec34043d0a7d91a2e4a20b07f5122440d2aeb
HYUNMIN-HWANG <hwangkei0212@gmail.com>
2021-02-18 17:09:29 +0900

    21-02-18 import

---
+++ python_import/p11_car.py
@@ -0,0 +1,7 @@
+def drive() :
+    print("운전하다.")
+
+drive()
+
+print("P11_car.py의 module 이름은 : ", __name__)    # __name__ : 모듈 이름을 알려준다.
+# P11_car.py의 module 이름은 :  __main__ <<  실행의 기준이 된다. 이 파일이 main이다.
---
+++ python_import/p12_tv.py
@@ -0,0 +1,7 @@
+def watch() :
+    print("시청하다.")
+
+watch()
+
+print("P12_tv.py의 module 이름은 : ", __name__)    # __name__ : 모듈 이름을 알려준다.
+# P12_tv.py의 module 이름은 :  __main__  << 실행의 기준이 된다. 이 파일이 main이다.
