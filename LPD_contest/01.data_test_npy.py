5638a462f61e807f34583201c44a4005364326d1
HYUNMIN-HWANG <hwangkei0212@gmail.com>
2021-03-17 20:47:27 +0900

    21-03-17

--- LPD_contest/01.data_test_npy.py
+++ LPD_contest/01.data_test_npy.py
@@ -33,24 +33,38 @@ print(len(test_image))      # 72000
 
 resize_data = []
 
-for img in test_image :
-    now_img = os.path.basename(img)
-    print("\n", now_img)
+# for img in test_image :
+#     now_img = os.path.basename(img)
+#     print("\n", now_img)
 
-    copy_img = img 
-    img1 = load_img(copy_img , color_mode='rgb', target_size=(128,128)) 
-    img1 = img1.resize((128, 128))
-    img1 = np.array(img1)
-    img1 = img1.reshape(-1, 128, 128,3)
-    # print(img1.shape)   # (1, 128, 128, 3)
-    resize_data.append(img1)
+#     copy_img = img 
+#     img1 = load_img(copy_img , color_mode='rgb', target_size=(128,128)) 
+#     img1 = img1.resize((128, 128))
+#     img1 = np.array(img1)
+#     img1 = img1.reshape(-1, 128, 128,3)
+#     # print(img1.shape)   # (1, 128, 128, 3)
+#     resize_data.append(img1)
+
+
+img1=[]
+for i in range(0,72000):
+    filepath='../lotte/test/%d.jpg'%i
+    image2 = Image.open(filepath)
+    image2 = image2.convert('RGB')
+    image2 = image2.resize((image_w, image_h))
+    image_data2=asarray(image2)
+    # image_data2 = signal.medfilt2d(np.array(image_data2), kernel_size=3)
+    img1.append(image_data2)    
+
+np.save("../data/lotte/npy/test.npy", arr=img1)
+
+x_pred = np.load('../data/lotte/npy/test.npy',allow_pickle=True)
 
 test_datagen = ImageDataGenerator()
 xy_test = test_datagen.flow(
     resize_data,
     batch_size=80000,
 )   
-
 
 np.save('../data/LPD_competition/npy/data_x1_test.npy', arr=xy_test)
 
